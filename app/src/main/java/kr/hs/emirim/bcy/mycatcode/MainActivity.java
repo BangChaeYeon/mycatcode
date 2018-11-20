1f8c44cf5edea7aa1096ccd4b3ffedc3cd652634
ChaeYeon Bang <qkdcodus0117@gmail.com>
2018-11-20 20:56:50 +0900

    #11 Add log to activities

--- app/src/main/java/kr/hs/emirim/bcy/mycatcode/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/bcy/mycatcode/MainActivity.java
@@ -5,29 +5,30 @@ package kr.hs.emirim.bcy.mycatcode;
         import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    final static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         System.out.println("Oncreate가 실행됨");
-        Log.v("MainActivity","Oncreate가 실행됨");
-        Log.v("MainActivity","Log.v 실행됨");
-        Log.i("MainActivity","Log.i 실행됨");
-        Log.d("MainActivity","Log.d 실행됨");
-        Log.w("MainActivity","Log.w 실행됨");
-        Log.e("MainActivity","Log.e 실행됨");
+        Log.v(TAG,"Oncreate가 실행됨");
+        Log.v(TAG,"Log.v 실행됨");
+        Log.i(TAG,"Log.i 실행됨");
+        Log.d(TAG,"Log.d 실행됨");
+        Log.w(TAG,"Log.w 실행됨");
+        Log.e(TAG,"Log.e 실행됨");
         setContentView(R.layout.activity_main);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.v("MainActivity","onResume이 실행됨");
+        Log.v(TAG,"onResume이 실행됨");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.v("MainActivity","onStart가 실행됨");
+        Log.v(TAG,"onStart가 실행됨");
     }
 }
--- app/src/main/java/kr/hs/emirim/bcy/mycatcode/SplashActivity.java
+++ app/src/main/java/kr/hs/emirim/bcy/mycatcode/SplashActivity.java
@@ -2,6 +2,7 @@ package kr.hs.emirim.bcy.mycatcode;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Log.v("MainActivity","SplashActivity가 실행됨");
     }
 }
